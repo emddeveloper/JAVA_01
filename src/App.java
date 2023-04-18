0bde5aa4e2bafa024b26879145b5f1c42d8ff809
emddeveloper <emd.developer@gmail.com>
2023-04-18 17:36:41 +0530

    lecture 3 conditional statements odd even check

--- src/App.java
+++ src/App.java
@@ -7,13 +7,20 @@ public class App {
         //switch
         //break
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        if(age >= 18){
-            System.out.println("Adult");
-        }
-        else {
-            System.out.println("Not Adult");
-        }
+//        int age = sc.nextInt();
+//        if(age >= 18){
+//            System.out.println("Adult");
+//        }
+//        else {
+//            System.out.println("Not Adult");
+//        }
+         int x = sc.nextInt();
+         if(x % 2 == 0){
+             System.out.println("EVEN NUMBER");
+         }
+         else {
+             System.out.println("ODD NUMBER");
+         }
 
     }
 }
