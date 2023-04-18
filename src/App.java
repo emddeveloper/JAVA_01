ce1b1c7d54c1da5d036a33ee198cbdea38f7a358
emddeveloper <emd.developer@gmail.com>
2023-04-18 16:47:10 +0530

    pattern print

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String x = "Hello world";
-        System.out.println(x);
+        System.out.print("* \n** \n*** \n****");
     }
 }
