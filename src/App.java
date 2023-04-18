121f29e198a9b516893a9b5c0cf847e1542309bf
emddeveloper <emd.developer@gmail.com>
2023-04-18 16:51:47 +0530

    Variables

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.print("* \n** \n*** \n****");
+        //System.out.print("* \n** \n*** \n****");
+        //Variables
+        String name ="Sk Aminul";
+        int a = 25;
+        int b = 10;
+        int age = 30;
+        double price = 25.50;
+
+        // We can change the value of variables
+        b = 30;
+        System.out.println(b); // Will print 30
     }
 }
