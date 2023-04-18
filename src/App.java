bb98b2e6e71354e824c71a3f028a65c0e5361d8a
emddeveloper <emd.developer@gmail.com>
2023-04-18 17:42:16 +0530

    lecture 3 conditional statements equal,greater lesser

--- src/App.java
+++ src/App.java
@@ -14,13 +14,22 @@ public class App {
 //        else {
 //            System.out.println("Not Adult");
 //        }
-         int x = sc.nextInt();
-         if(x % 2 == 0){
-             System.out.println("EVEN NUMBER");
-         }
-         else {
-             System.out.println("ODD NUMBER");
-         }
+//         int x = sc.nextInt();
+//         if(x % 2 == 0){
+//             System.out.println("EVEN NUMBER");
+//         }
+//         else {
+//             System.out.println("ODD NUMBER");
+//         }
 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a==b){
+            System.out.println("equal");
+        } else if (a>b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("a is lesser than b");
+        }
     }
 }
